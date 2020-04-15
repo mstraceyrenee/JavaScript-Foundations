d47e9ea2bb431cfdade457da317aea012b0f7ffe
Tracey Cooper <traceystreasuretrove@gmail.com>
2020-04-15 16:55:16 -0400

    Completes task 1

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
 // 🏡 Task 1: Variables
 /* Create variables for principal, interest rate, and years. Assign them the values 200000, 0.05, and 30 respectively. Create another value called name and give it the value of your own name.
 */
+let principal = "200000";
+let interestRate = "0.05";
+let years = "30";
+const name = "Tracey";
 
-
+console.log(name);
 
 
 
